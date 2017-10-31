2f0c1de7573d072d9a77d34f3a0a9dffddc0bc1b
erni <erni.fekete@reversinglabs.com>
2017-10-31 15:25:04 +0100

    copyright changed to ReversingLab

---
+++ tiscale_consts.py
@@ -0,0 +1,35 @@
+# --
+#
+# Copyright (c) ReversingLabs Inc 2016Copyright (c) ReversingLabs Inc 2016-2017
+#
+# This unpublished material is proprietary to ReversingLabs Inc.
+# All rights reserved.
+# Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of ReversingLabs Inc.
+#
+# --
+
+TISCALE_JSON_BASE_URL = "base_url"
+TISCALE_JSON_TASK_ID = "task_id"
+TISCALE_JSON_API_KEY = "api_key"
+TISCALE_JSON_MALWARE = "malware"
+TISCALE_JSON_TASK_ID = "id"
+TISCALE_JSON_URL = "url"
+TISCALE_JSON_HASH = "hash"
+TISCALE_JSON_PLATFORM = "platform"
+TISCALE_JSON_POLL_TIMEOUT_MINS = "timeout"
+
+TISCALE_ERR_UNABLE_TO_PARSE_REPLY = "Unable to parse reply from device"
+TISCALE_ERR_REPLY_FORMAT_KEY_MISSING = "None '{key}' missing in reply from device"
+TISCALE_ERR_REPLY_NOT_SUCCESS = "REST call returned '{status}'"
+TISCALE_SUCC_REST_CALL_SUCCEEDED = "REST Api call succeeded"
+TISCALE_ERR_REST_API = "REST Api Call returned error, status_code: {status_code}, detail: {detail}"
+
+TISCALE_TEST_PDF_FILE = "tiscale_test_connectivity.pdf"
+TISCALE_SLEEP_SECS = 10
+TISCALE_MSG_REPORT_PENDING = "Report Pending"
+TISCALE_MSG_MAX_POLLS_REACHED = "Reached max polling attempts. Please use the MD5 or Sha256 of the file as a parameter to <b>get report</b> to query the report status."
+
+# in minutes
+TISCALE_MAX_TIMEOUT_DEF = 10
